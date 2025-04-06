001f4a8dbc7d5799d6b08f95a2699c6d2c37d313
abimanyu-g <abimanyu.g@vvdntech.in>
2025-04-06 13:03:37 +0530

    Data transformation implementation

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys
 
 from networksecurity.entity.config_entity import TrainPipelineConfig
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
 if __name__=="__main__":
     try:
+        ## train pipeline config initialised 
         trainpipelineconfig=TrainPipelineConfig()
+
+        ### DATA INGESTION ###
         data_ingestionconfig=DataIngestionConfig(trainpipelineconfig)
         data_ingestion=DataIngestion(data_ingestionconfig)
         logging.info("Initiated the data ingestion")
@@ -21,14 +25,24 @@ if __name__=="__main__":
 
         logging.info("Data ingestion completed")
 
+        ### DATA VALIDATION ###
         data_validation_config=DataValidationConfig(trainpipelineconfig)
         data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
         logging.info("Initiated the data validation")
-
         data_validation_artifact=data_validation.initiate_data_validation()
-        logging.info("Data validation completed")
-
         print(data_validation_artifact)
+        logging.info("Data validation completed")
+        
+        ### DATA TRANSFORMATION ###
+        data_transformation_config = DataTransformationConfig(trainpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact
+                           ,data_transformation_config=data_transformation_config)
+        logging.info("Initiated data transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data transformation completed")
+
+
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 
 """
 Defining all common constant variable for training pipeline
@@ -35,3 +35,18 @@ DATA_VALIDATION_INVALID_DIR:str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yaml"
 
+
+"""
+Data transformation related constant, start with DATA_TRANSFORMATION VAR name 
+"""
+
+DATA_TRANSFORMATION_DIR_NAME:str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str = "transformed_object"
+PREPROCESSING_OBJECT_FILE_NAME:str = "preprocessor.pkl"
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS:dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform"
+}
