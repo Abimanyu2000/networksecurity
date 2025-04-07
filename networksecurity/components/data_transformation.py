368a4fe23180d692891fad5b76aac75c40f0331c
abimanyu-g <abimanyu.g@vvdntech.in>
2025-04-07 18:52:09 +0530

    pipeline implementation completed

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -90,6 +90,7 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformaton_config.transformed_test_file_path, array=test_arr)
             save_object(self.data_transformaton_config.transformed_object_file_path, obj=preprocessor_obj)
 
+            save_object("final_model/preprocessor.pkl", preprocessor_obj)
 
             #preparing artifacts
 
--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -115,6 +115,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor, model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=Network_Model)
+        
+        save_object("final_model/model.pkl", best_model)
 
         #Model Trainer Artifact
 
